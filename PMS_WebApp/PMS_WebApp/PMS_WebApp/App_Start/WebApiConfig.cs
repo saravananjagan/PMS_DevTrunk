7ec3ad1fa4298a328751d77a6cdff0297606f208
saravananjagan <43237439+saravananjagan@users.noreply.github.com>
2018-09-30 18:59:39 +0530

    PMS_WebApp ExaminationType Module Creation

---
+++ PMS_WebApp/PMS_WebApp/PMS_WebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using PMS_WebApp.Models;
+
+namespace PMS_WebApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<ExaminationType>("ExaminationTypes");
+            builder.EntitySet<Customer>("Customers");
+            builder.EntitySet<ExaminationSchedule>("ExaminationSchedules");
+            builder.EntitySet<ModuleDetail>("ModuleDetails");
+            builder.EntitySet<ModuleMapping>("ModuleMappings");
+            builder.EntitySet<ExaminationSchedule>("ExaminationSchedules");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+        }
+    }
+}
+    
---
+++ PMS_WebApp/PMS_WebApp/PMS_WebApp/Controllers/ExaminationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS_WebApp.Controllers
+{
+    public class ExaminationController : Controller
+    {
+        // GET: Examination
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: ExaminationConfig
+        public ActionResult ExaminationConfig()
+        {
+            return View();
+        }
+    }
+}
